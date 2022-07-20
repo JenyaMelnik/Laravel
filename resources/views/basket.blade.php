94f4ab1f73c4c914fd6fc609c8af8ceff6521f1f
JenyaMelnik <Jenyamelnik1986@i.ua>
2022-07-20 18:26:26 +0300

    feat:lesson 7

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -23,15 +23,24 @@
                         </a>
                     </td>
                     <td>
+                        <span>{{ $product->pivot->count }}</span>
                         <form action="{{ route('basket-add', $product) }}" method="POST">
                             <button type="submit"> + </button>
                             @csrf
                         </form>
+                        <form action="{{ route('basket-remove', $product) }}" method="POST">
+                            <button type="submit"> - </button>
+                            @csrf
+                        </form>
                     </td>
                     <td> {{ $product->price }}</td>
-                    <td> {{ $product->price }}</td>
+                    <td> {{ $product->getPriceForCount() }}</td>
                 </tr>
             @endforeach
+            <tr>
+                <td colspan="3">Общая стоимость:</td>
+                <td>{{$order->getFullPrice()}}</td>
+            </tr>
             </tbody>
         </table>
     </div>
